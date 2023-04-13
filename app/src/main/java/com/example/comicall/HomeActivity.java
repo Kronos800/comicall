291e674db13d4ef2e7ab7282ea25fc077395a9f7
jesus <jesusgape08@gmail.com>
2023-04-13 13:42:31 +0200

    Empezada backend del feed

--- app/src/main/java/com/example/comicall/HomeActivity.java
+++ app/src/main/java/com/example/comicall/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import androidx.loader.app.LoaderManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,17 +13,25 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ComicLoaderCallbacks comicLoaderCallbacks;
+    private static final int COMIC_LOADER_ID = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Intent anteriorIntent = getIntent();
         Bundle bundle = anteriorIntent.getExtras();
+        comicLoaderCallbacks = new ComicLoaderCallbacks(this);
+        LoaderManager loaderManager = LoaderManager.getInstance(this);
+        if(loaderManager.getLoader(COMIC_LOADER_ID) != null){
+            loaderManager.initLoader(COMIC_LOADER_ID,null,  comicLoaderCallbacks);
+        }
         setup(bundle.getString("email"));
     }
 
     private void setup(String email){
-        TextView emailTextView = findViewById(R.id.emailTextView);
+       /* TextView emailTextView = findViewById(R.id.emailTextView);
         emailTextView.setText(email);
         Button logOut = findViewById(R.id.logOutButton);
         logOut.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +40,6 @@ public class HomeActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 onBackPressed();
             }
-        });
+        });*/
     }
 }
