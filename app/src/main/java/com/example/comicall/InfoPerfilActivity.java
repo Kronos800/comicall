e10f06b5d2d2e585f3a604966fc6cfd663b0712b
Michael Paredes Sanchez <michaelsteven2000@hotmail.com>
2023-04-20 13:05:38 +0200

    actualizada el perfil de usuario (landscape puede dar problemas)

---
+++ app/src/main/java/com/example/comicall/InfoPerfilActivity.java
@@ -0,0 +1,54 @@
+package com.example.comicall;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class InfoPerfilActivity extends AppCompatActivity {
+
+    private ImageView imagenPerfil;
+    private TextView nombreUsuario;
+    private TextView correoUsuario;
+
+    private Button cerrarSesionButton;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_perfil);
+
+        //mAuth = FirebaseAuth.getInstance();
+
+        imagenPerfil = findViewById(R.id.profile_image);
+        nombreUsuario = findViewById(R.id.profile_username);
+        correoUsuario = findViewById(R.id.profile_email);
+        cerrarSesionButton = findViewById(R.id.signoutbutton);
+
+
+        //mirar como sacar de la base de datos la imagen, nombre y correo
+
+        imagenPerfil.setImageResource(R.drawable.icono_logo_comicall);//CAMBIAR A LOS DATOS DE LA BASE DE DATOS
+        nombreUsuario.setText("Michael Paredes Sanchez");
+        correoUsuario.setText("michael.paredes@gmail.com");
+
+        cerrarSesionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(InfoPerfilActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
+
